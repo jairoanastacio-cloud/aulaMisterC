14b8d0e90dec9b1d95d1f97c8daadc475d56fe87
jairoanastacio-cloud <jairo.anastacio@escola.pr.gov.br>
2026-05-12 10:06:05 -0300

    codigos resolvidos

---
+++ atv1.py
@@ -0,0 +1,3 @@
+alunos = ["Richard","Marcos","Oto","Paulo","Molly"]
+print(alunos[0])
+print(alunos[-1])
---
+++ atv4.py
@@ -0,0 +1,8 @@
+pares = []
+
+for n in range(1,21):
+    if n % 2 == 0:
+        pares.append(n)
+
+print(pares)
+print(len(pares))
