36cdb0e0564aa643df4d2a46af41e3f509c0ca0d
jairoanastacio-cloud <jairo.anastacio@escola.pr.gov.br>
2026-06-02 20:58:10 -0300

    Add files via upload

---
+++ aula0106/atv4jp.py
@@ -0,0 +1,27 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade 
+
+class Aluno(Pessoa):
+    def __init__(self, nome, idade, matricula):
+        super().__init__(nome, idade)
+        self.matricula = matricula
+    def apresentar(self):
+        print(f"Aluno: {self.nome}, Idade: {self.idade}, Matricula: {self.matricula}")
+
+
+class Professor(Pessoa):
+    def __init__(self, nome, idade, salario):
+        super().__init__(nome, idade)
+        self.salario = salario
+    def apresentar(self):
+        print(f"Professor: {self.nome}, Idade: {self.idade}, Salario: {self.salario}")
+
+aluno = Aluno("William", 982, 7)
+prof = Professor("Vitor Felipe Kogisteski Magalhães da Silva Santos Programador PHP Junior e Programador CObol Senior e Jogador pela seleção Daiana do Paraná",9980 , 2026)
+
+lista_pessoa = [aluno, prof]
+
+for pessoa in lista_pessoa:
+    pessoa.apresentar()
