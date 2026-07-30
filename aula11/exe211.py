45ff41e7d816555b497e514173306e476482d232
jairoanastacio-cloud <jairo.anastacio@escola.pr.gov.br>
2026-07-30 14:45:48 -0300

    Add files via upload

---
+++ aula11/exe211.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conexao = sqlite3.connect("loja.db")
+conexao.row_factory = sqlite3.Row 
+cursor = conexao.cursor()
+
+cursor.execute("SELECT * FROM produtos")
+produtos = cursor.fetchall()  
+
+for produto in produtos:
+    print(dict(produto))
+
+conexao.close()
